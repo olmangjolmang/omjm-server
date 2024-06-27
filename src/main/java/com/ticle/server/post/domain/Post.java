b9d09e9492dc2b3337206ae221e726c840770f15
Gongchaeyeon <dnjftjd53@kookmin.ac.kr>
2024-06-27 13:25:45 +0900

    🗃️ Chore: 이미지, 날짜 global에 선언한 Type으로 변경

--- src/main/java/com/ticle/server/post/domain/Post.java
+++ src/main/java/com/ticle/server/post/domain/Post.java
@@ -1,11 +1,15 @@
 package com.ticle.server.post.domain;
 
+import com.ticle.server.global.domain.BaseTimeEntity;
+import com.ticle.server.global.domain.S3Info;
 import com.ticle.server.mypage.domain.User;
 import com.ticle.server.mypage.domain.type.Category;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 
 @Table(name = "Post")
@@ -30,15 +34,16 @@ public class Post {
     @Column(name="author",nullable = false)
     private String author;
 
+    @CreatedDate
     @Column(name="create_date",nullable = false)
-    private Timestamp create_date;
+    private LocalDateTime createdDate;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "post_category")
     private Category post_category;
 
     @Column(name="image")
-    private String image;
+    private S3Info image;
 
     @ManyToOne
     @JoinColumn(name="user_id")
