0546736a2d883a4998b5cc64989b2a464a95e35b
Amepistheo <kellylhji@gmail.com>
2024-06-27 14:30:46 +0900

    :sparkles: Feat: 구독서비스 DB 구축

---
+++ src/main/java/com/ticle/server/home/domain/Subscription.java
@@ -0,0 +1,43 @@
+package com.ticle.server.home.domain;
+
+import com.ticle.server.global.domain.BaseTimeEntity;
+import com.ticle.server.home.domain.type.Day;
+import com.ticle.server.mypage.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "subscription")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Subscription extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subs_id")
+    private Long subsId;
+
+    @Column(name = "agree_info")
+    private Boolean agreeInfo;
+
+    @Column(name = "agree_marketing")
+    private Boolean agreeMarketing;
+
+    @Column(name = "subs_day")
+    private Day subsDay;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Builder
+    public Subscription(Boolean agreeInfo, Boolean agreeMarketing, Day subsDay, User user) {
+        this.agreeInfo = agreeInfo;
+        this.agreeMarketing = agreeMarketing;
+        this.subsDay = subsDay;
+        this.user = user;
+    }
+}
