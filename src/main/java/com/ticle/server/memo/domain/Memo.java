d013c97d066a80f429fb77f06cec698b297b6360
Gongchaeyeon <dnjftjd53@kookmin.ac.kr>
2024-06-27 13:35:38 +0900

    ✏️ Chore: Post와 연결된 부분 주석 해제

--- src/main/java/com/ticle/server/memo/domain/Memo.java
+++ src/main/java/com/ticle/server/memo/domain/Memo.java
@@ -1,6 +1,7 @@
 package com.ticle.server.memo.domain;
 
 import com.ticle.server.mypage.domain.User;
+import com.ticle.server.post.domain.Post;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -29,9 +30,8 @@ public class Memo {
     @JoinColumn(name="user_id")
     private User user;
 
-    // post 머지 후에 주석 풀어야 함.
-//    @ManyToOne
-//    @JoinColumn(name="post_id")
-//    private Post post;
+    @ManyToOne
+    @JoinColumn(name="post_id")
+    private Post post;
 
 }
