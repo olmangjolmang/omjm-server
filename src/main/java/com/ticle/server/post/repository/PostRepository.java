9feddf21735b252478078e64f0cf2eb843a44fd2
Gongchaeyeon <dnjftjd53@kookmin.ac.kr>
2024-07-02 16:09:35 +0900

    🐛 Fix: Category import Error 수정

--- src/main/java/com/ticle/server/post/repository/PostRepository.java
+++ src/main/java/com/ticle/server/post/repository/PostRepository.java
@@ -1,13 +1,13 @@
 package com.ticle.server.post.repository;
 
-import com.ticle.server.mypage.domain.type.Category;
+
 import com.ticle.server.post.domain.Post;
+import com.ticle.server.user.domain.type.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByCategory(Category category);
-    
 
 }
