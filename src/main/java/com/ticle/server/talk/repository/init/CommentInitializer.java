31b9ac3ce1b8c485ee9157ae76c0990dfe366cb4
Amepistheo <kellylhji@gmail.com>
2024-07-04 01:52:17 +0900

    :pencil2: Chore: 변수명 CamelCase로 변경

--- src/main/java/com/ticle/server/talk/repository/init/CommentInitializer.java
+++ src/main/java/com/ticle/server/talk/repository/init/CommentInitializer.java
@@ -29,101 +29,101 @@ public class CommentInitializer implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         if (commentRepository.count() > 0) {
-            log.info("[Talk] 더미 데이터 존재");
+            log.info("[Comment] 더미 데이터 존재");
         } else {
-            User DUMMY_USER = userRepository.findById(2L).orElseThrow();
-            User DUMMY_USER2 = userRepository.findById(3L).orElseThrow();
-            User DUMMY_USER3 = userRepository.findById(4L).orElseThrow();
-            User DUMMY_USER4 = userRepository.findById(5L).orElseThrow();
-            User DUMMY_USER5 = userRepository.findById(6L).orElseThrow();
+            User dummyUser = userRepository.findById(2L).orElseThrow();
+            User dummyUser2 = userRepository.findById(3L).orElseThrow();
+            User dummyUser3 = userRepository.findById(4L).orElseThrow();
+            User dummyUser4 = userRepository.findById(5L).orElseThrow();
+            User dummyUser5 = userRepository.findById(6L).orElseThrow();
 
-            Talk TALK1 = talkRepository.findByTalkId(1L).orElseThrow();
-            Talk TALK2 = talkRepository.findByTalkId(2L).orElseThrow();
+            Talk talk1 = talkRepository.findById(1L).orElseThrow();
+            Talk talk2 = talkRepository.findById(2L).orElseThrow();
 
 
             List<Comment> commentList = new ArrayList<>();
 
-            Comment COMMENT1 = Comment.builder()
-                    .talk(TALK1)
+            Comment comment1 = Comment.builder()
+                    .talk(talk1)
                     .content("좋은 리더에게는 리더십이 필요해")
                     .heartCount(5L)
-                    .user(DUMMY_USER)
+                    .user(dummyUser)
                     .build();
 
-            Comment COMMENT2 = Comment.builder()
-                    .talk(TALK1)
+            Comment comment2 = Comment.builder()
+                    .talk(talk1)
                     .content("리더는 소통 능력이 중요하다고 생각해")
                     .heartCount(8L)
-                    .user(DUMMY_USER2)
+                    .user(dummyUser2)
                     .build();
 
-            Comment COMMENT3 = Comment.builder()
-                    .talk(TALK1)
+            Comment comment3 = Comment.builder()
+                    .talk(talk1)
                     .content("책임감과 결단력이 있어야 해")
                     .heartCount(10L)
-                    .user(DUMMY_USER3)
+                    .user(dummyUser3)
                     .build();
 
-            Comment COMMENT4 = Comment.builder()
-                    .talk(TALK1)
+            Comment comment4 = Comment.builder()
+                    .talk(talk1)
                     .content("팀원들의 의견을 존중하는 리더가 필요해")
                     .heartCount(7L)
-                    .user(DUMMY_USER4)
+                    .user(dummyUser4)
                     .build();
 
-            Comment COMMENT5 = Comment.builder()
-                    .talk(TALK1)
+            Comment comment5 = Comment.builder()
+                    .talk(talk1)
                     .content("문제를 해결하는 능력도 중요하지")
                     .heartCount(6L)
-                    .user(DUMMY_USER5)
+                    .user(dummyUser5)
                     .build();
 
             // "팀에서 가장 중요한 구성원은 누구인가요?"에 대한 댓글 5개
-            Comment COMMENT6 = Comment.builder()
-                    .talk(TALK2)
+            Comment comment6 = Comment.builder()
+                    .talk(talk2)
                     .content("모두가 중요한 역할을 하지만, 프로젝트 매니저가 핵심이라고 생각해")
                     .heartCount(5L)
-                    .user(DUMMY_USER4)
+                    .user(dummyUser4)
                     .build();
 
-            Comment COMMENT7 = Comment.builder()
-                    .talk(TALK2)
+            Comment comment7 = Comment.builder()
+                    .talk(talk2)
                     .content("팀의 기술 리더가 가장 중요한 구성원이라고 생각해")
                     .heartCount(8L)
-                    .user(DUMMY_USER)
+                    .user(dummyUser)
                     .build();
 
-            Comment COMMENT8 = Comment.builder()
-                    .talk(TALK2)
+            Comment comment8 = Comment.builder()
+                    .talk(talk2)
                     .content("커뮤니케이션을 담당하는 사람이 중요하다고 봐")
                     .heartCount(10L)
-                    .user(DUMMY_USER5)
+                    .user(dummyUser5)
                     .build();
 
-            Comment COMMENT9 = Comment.builder()
-                    .talk(TALK2)
+            Comment comment9 = Comment.builder()
+                    .talk(talk2)
                     .content("모든 팀원이 각자의 역할을 충실히 하는 것이 가장 중요해")
                     .heartCount(7L)
-                    .user(DUMMY_USER2)
+                    .user(dummyUser2)
                     .build();
 
-            Comment COMMENT10 = Comment.builder()
-                    .talk(TALK2)
+            Comment comment10 = Comment.builder()
+                    .talk(talk2)
                     .content("팀워크를 이끌어가는 리더가 가장 중요한 구성원이지")
                     .heartCount(6L)
-                    .user(DUMMY_USER3)
+                    .user(dummyUser3)
                     .build();
 
-            commentList.add(COMMENT1);
-            commentList.add(COMMENT2);
-            commentList.add(COMMENT3);
-            commentList.add(COMMENT4);
-            commentList.add(COMMENT5);
-            commentList.add(COMMENT6);
-            commentList.add(COMMENT7);
-            commentList.add(COMMENT8);
-            commentList.add(COMMENT9);
-            commentList.add(COMMENT10);
+            commentList.add(comment1);
+            commentList.add(comment2);
+            commentList.add(comment3);
+            commentList.add(comment4);
+            commentList.add(comment5);
+            commentList.add(comment6);
+            commentList.add(comment7);
+            commentList.add(comment8);
+            commentList.add(comment9);
+            commentList.add(comment10);
 
             commentRepository.saveAll(commentList);
         }
--- src/main/java/com/ticle/server/talk/repository/init/TalkInitializer.java
+++ src/main/java/com/ticle/server/talk/repository/init/TalkInitializer.java
@@ -28,123 +28,122 @@ public class TalkInitializer implements ApplicationRunner {
         if (talkRepository.count() > 0) {
             log.info("[Talk] 더미 데이터 존재");
         } else {
-            User DUMMY_ADMIN = userRepository.findById(1L).orElseThrow();
+            User dummyAdmin = userRepository.findById(1L).orElseThrow();
 
             List<Talk> talkList = new ArrayList<>();
 
-            Talk TALK1 = Talk.builder()
+            Talk talk1 = Talk.builder()
                     .question("좋은 리더란 어떤 자질이 필요할까요?")
-                    .view(5L)
+                    .viewCount(5L)
                     .commentCount(4L)
-                    .user(DUMMY_ADMIN)
+                    .user(dummyAdmin)
                     .build();
 
-            Talk TALK2 = Talk.builder()
+            Talk talk2 = Talk.builder()
                     .question("팀에서 가장 중요한 구성원은 누구인가요?")
-                    .view(10L)
+                    .viewCount(10L)
                     .commentCount(3L)
-                    .user(DUMMY_ADMIN)
+                    .user(dummyAdmin)
                     .build();
 
-            Talk TALK3 = Talk.builder()
+            Talk talk3 = Talk.builder()
                     .question("롤모델이 있다면 누구이고 이유는 무엇인가요?")
-                    .view(14L)
+                    .viewCount(14L)
                     .commentCount(5L)
-                    .user(DUMMY_ADMIN)
+                    .user(dummyAdmin)
                     .build();
 
-            Talk TALK4 = Talk.builder()
+            Talk talk4 = Talk.builder()
                     .question("인생에서 가장 필요한 사항은 무엇이라 생각하나요?")
-                    .view(20L)
+                    .viewCount(20L)
                     .commentCount(7L)
-                    .user(DUMMY_ADMIN)
+                    .user(dummyAdmin)
                     .build();
 
-            Talk TALK5 = Talk.builder()
+            Talk talk5 = Talk.builder()
                     .question("대인관계에서 가장 중요하게 생각하는 것은 무엇인가요?")
-                    .view(4L)
+                    .viewCount(4L)
                     .commentCount(2L)
-                    .user(DUMMY_ADMIN)
+                    .user(dummyAdmin)
                     .build();
 
-            Talk TALK6 = Talk.builder()
+            Talk talk6 = Talk.builder()
                     .question("가장 기억에 남는 프로젝트는 무엇인가요?")
-                    .view(12L)
+                    .viewCount(12L)
                     .commentCount(6L)
-                    .user(DUMMY_ADMIN)
+                    .user(dummyAdmin)
                     .build();
 
-            Talk TALK7 = Talk.builder()
+            Talk talk7 = Talk.builder()
                     .question("성공적인 팀워크를 위해 가장 필요한 요소는 무엇인가요?")
-                    .view(8L)
+                    .viewCount(8L)
                     .commentCount(3L)
-                    .user(DUMMY_ADMIN)
+                    .user(dummyAdmin)
                     .build();
 
-            Talk TALK8 = Talk.builder()
+            Talk talk8 = Talk.builder()
                     .question("기술 발전이 사회에 미치는 영향은 무엇인가요?")
-                    .view(15L)
+                    .viewCount(15L)
                     .commentCount(4L)
-                    .user(DUMMY_ADMIN)
+                    .user(dummyAdmin)
                     .build();
 
-            Talk TALK9 = Talk.builder()
+            Talk talk9 = Talk.builder()
                     .question("효과적인 문제 해결 방법은 무엇이라 생각하나요?")
-                    .view(9L)
+                    .viewCount(9L)
                     .commentCount(2L)
-                    .user(DUMMY_ADMIN)
+                    .user(dummyAdmin)
                     .build();
 
-            Talk TALK10 = Talk.builder()
+            Talk talk10 = Talk.builder()
                     .question("업무에서 동기부여를 유지하는 방법은 무엇인가요?")
-                    .view(18L)
+                    .viewCount(18L)
                     .commentCount(5L)
-                    .user(DUMMY_ADMIN)
+                    .user(dummyAdmin)
                     .build();
 
-            Talk TALK11 = Talk.builder()
+            Talk talk11 = Talk.builder()
                     .question("IT 업계에서 가장 중요한 기술 트렌드는 무엇인가요?")
-                    .view(11L)
+                    .viewCount(11L)
                     .commentCount(3L)
-                    .user(DUMMY_ADMIN)
+                    .user(dummyAdmin)
                     .build();
 
-            Talk TALK12 = Talk.builder()
+            Talk talk12 = Talk.builder()
                     .question("효과적인 의사소통 방법은 무엇이라고 생각하나요?")
-                    .view(7L)
+                    .viewCount(7L)
                     .commentCount(4L)
-                    .user(DUMMY_ADMIN)
+                    .user(dummyAdmin)
                     .build();
 
-            Talk TALK13 = Talk.builder()
+            Talk talk13 = Talk.builder()
                     .question("최근에 배운 가장 중요한 교훈은 무엇인가요?")
-                    .view(10L)
+                    .viewCount(10L)
                     .commentCount(5L)
-                    .user(DUMMY_ADMIN)
+                    .user(dummyAdmin)
                     .build();
 
-            Talk TALK14 = Talk.builder()
+            Talk talk14 = Talk.builder()
                     .question("미래의 커리어 목표는 무엇인가요?")
-                    .view(13L)
+                    .viewCount(13L)
                     .commentCount(6L)
-                    .user(DUMMY_ADMIN)
+                    .user(dummyAdmin)
                     .build();
 
-
-            talkList.add(TALK1);
-            talkList.add(TALK2);
-            talkList.add(TALK3);
-            talkList.add(TALK4);
-            talkList.add(TALK5);
-            talkList.add(TALK6);
-            talkList.add(TALK7);
-            talkList.add(TALK8);
-            talkList.add(TALK9);
-            talkList.add(TALK10);
-            talkList.add(TALK11);
-            talkList.add(TALK12);
-            talkList.add(TALK13);
-            talkList.add(TALK14);
+            talkList.add(talk1);
+            talkList.add(talk2);
+            talkList.add(talk3);
+            talkList.add(talk4);
+            talkList.add(talk5);
+            talkList.add(talk6);
+            talkList.add(talk7);
+            talkList.add(talk8);
+            talkList.add(talk9);
+            talkList.add(talk10);
+            talkList.add(talk11);
+            talkList.add(talk12);
+            talkList.add(talk13);
+            talkList.add(talk14);
 
             talkRepository.saveAll(talkList);
         }
