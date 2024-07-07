416d000d939ad6a259d3e8c272bfb5d4d1159537
Amepistheo <kellylhji@gmail.com>
2024-07-07 11:47:54 +0900

    :sparkles: Feat: 이메일 유효성 검사 기능 구현

--- src/main/java/com/ticle/server/home/controller/HomeController.java
+++ src/main/java/com/ticle/server/home/controller/HomeController.java
@@ -1,4 +1,31 @@
 package com.ticle.server.home.controller;
 
+import com.ticle.server.global.dto.ResponseTemplate;
+import com.ticle.server.home.service.HomeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "Home", description = "홈 화면 관련 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/home")
 public class HomeController {
+
+    private final HomeService homeService;
+
+    @Operation(summary = "이메일 검증", description = "이메일 유효성 검증")
+    @GetMapping("/email/{email}")
+    public ResponseEntity<ResponseTemplate<Object>> validateEmail(
+            @PathVariable String email) {
+
+        Boolean response = homeService.validateEmail(email);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ResponseTemplate.from(response));
+    }
 }
--- src/main/java/com/ticle/server/home/service/HomeService.java
+++ src/main/java/com/ticle/server/home/service/HomeService.java
@@ -1,4 +1,20 @@
 package com.ticle.server.home.service;
 
+import com.ticle.server.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class HomeService {
+
+    private final UserRepository userRepository;
+
+    public Boolean validateEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
 }
