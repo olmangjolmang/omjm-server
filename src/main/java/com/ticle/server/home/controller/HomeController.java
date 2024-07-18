bf85e11081697404ef46268f2f684b83050e4c91
Amepistheo <kellylhji@gmail.com>
2024-07-19 00:05:23 +0900

    :bug: Fix: import 안된 부분 추가

--- src/main/java/com/ticle/server/home/controller/HomeController.java
+++ src/main/java/com/ticle/server/home/controller/HomeController.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 import static com.ticle.server.global.dto.ResponseTemplate.EMPTY_RESPONSE;
 
