616e547cc484cf973ed7a1d34950717757f1c3d8
Amepistheo <kellylhji@gmail.com>
2024-07-18 15:49:34 +0900

    :pencil2: Chore: LocalDateTime -> LocalDate로 변경 적용

--- src/main/java/com/ticle/server/home/dto/response/PostSetsResponse.java
+++ src/main/java/com/ticle/server/home/dto/response/PostSetsResponse.java
@@ -3,14 +3,14 @@ package com.ticle.server.home.dto.response;
 import com.ticle.server.post.domain.Post;
 import com.ticle.server.user.domain.type.Category;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public record PostSetsResponse(
         String title,
         String imageUrl,
         Category category,
         String author,
-        LocalDateTime createdDate
+        LocalDate createdDate
 ) {
     public static PostSetsResponse of(Post post) {
         return new PostSetsResponse(
