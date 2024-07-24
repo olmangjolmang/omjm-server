29218677ed8097b19f8737764954aca958cb1846
chaeyeon <dnjftjd53@kookmin.ac.kr>
2024-07-24 19:25:03 +0900

    ♻️ Refactor: post detail과 함께 읽으면 좋을 아티클 추천 분리 #52

---
+++ src/main/java/com/ticle/server/post/dto/ReadRecommendPost.java
@@ -0,0 +1,21 @@
+package com.ticle.server.post.dto;
+
+import com.ticle.server.post.domain.Post;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReadRecommendPost {
+    private List recommendPost;
+
+    public static ReadRecommendPost from(Post post) {
+        return new ReadRecommendPost(
+                post.getRecommendPost());
+    }
+
+}
