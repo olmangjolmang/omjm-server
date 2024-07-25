aee1210c8a19367b5573363ec49eeaf61569cdc8
Amepistheo <kellylhji@gmail.com>
2024-07-25 15:40:31 +0900

    :heavy_plus_sign: Add: 유저 프로필 정보에 카테고리 추가

--- src/main/java/com/ticle/server/user/dto/response/UserInfoResponse.java
+++ src/main/java/com/ticle/server/user/dto/response/UserInfoResponse.java
@@ -1,17 +1,22 @@
 package com.ticle.server.user.dto.response;
 
 import com.ticle.server.user.domain.User;
+import com.ticle.server.user.domain.type.Category;
 import lombok.Builder;
+
+import java.util.List;
 
 @Builder
 public record UserInfoResponse(
         String nickname,
-        String email
+        String email,
+        List<Category> category
 ) {
     public static UserInfoResponse from(User user) {
         return UserInfoResponse.builder()
                 .nickname(user.getNickName())
                 .email(user.getEmail())
+                .category(user.getCategory())
                 .build();
     }
 }
