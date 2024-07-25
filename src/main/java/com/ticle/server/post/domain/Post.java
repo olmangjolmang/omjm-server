ac738f95af5402fa6c2fb72713a62ea8f827735d
Amepistheo <kellylhji@gmail.com>
2024-07-25 22:10:14 +0900

    :bug: Fix: Content에 긴 내용 저장할 수 있게 변경

--- src/main/java/com/ticle/server/post/domain/Post.java
+++ src/main/java/com/ticle/server/post/domain/Post.java
@@ -28,7 +28,7 @@ public class Post {
     @Column(name = "title", nullable = false)
     private String title;
 
-    @Column(name = "content", nullable = false)
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
     private String content;
 
     @Column(name = "author", nullable = false)
